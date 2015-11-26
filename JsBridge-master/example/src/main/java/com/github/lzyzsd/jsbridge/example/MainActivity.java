0ea8f022c5240cc6370316a643b68ed55aae911c
亭 <353326403@qq.com>
2015-11-26 14:33:32 +0800

    no message

---
+++ JsBridge-master/example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
@@ -0,0 +1,147 @@
+package com.github.lzyzsd.jsbridge.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.widget.Button;
+
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.github.lzyzsd.jsbridge.DefaultHandler;
+import com.google.gson.Gson;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private final String TAG = "MainActivity";
+
+	BridgeWebView webView;
+
+	Button button;
+
+	int RESULT_CODE = 0;
+
+	ValueCallback<Uri> mUploadMessage;
+
+    static class Location {
+        String address;
+    }
+
+    static class User {
+        String name;
+        Location location;
+        String testStr;
+    }
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+        webView = (BridgeWebView) findViewById(R.id.webView);
+
+//		button = (Button) findViewById(R.id.button);
+
+//		button.setOnClickListener(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAppCacheMaxSize(1024*1024*8);
+        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+        webView.getSettings().setAppCachePath(appCachePath);
+        webView.getSettings().setAppCacheEnabled(true);
+
+        webView.setDefaultHandler(new DefaultHandler());
+
+		webView.setWebChromeClient(new WebChromeClient() {
+
+			@SuppressWarnings("unused")
+			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
+				this.openFileChooser(uploadMsg);
+			}
+
+			@SuppressWarnings("unused")
+			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
+				this.openFileChooser(uploadMsg);
+			}
+
+			public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+				mUploadMessage = uploadMsg;
+			}
+		});
+
+		webView.loadUrl("file:///android_asset/index.html");
+
+//        webView.callHandler("selectImage", new Gson().toJson(user), new CallBackFunction() {
+//            @Override
+//            public void onCallBack(String data) {
+//                pickFile();
+//            }
+//        });
+
+		webView.registerHandler("selectImageHandler", new BridgeHandler() {
+
+			@Override
+			public void handler(String data, CallBackFunction function) {
+                pickFile();
+                function.onCallBack(data);
+			}
+
+		});
+
+//        User user = new User();
+//        Location location = new Location();
+//        location.address = "SDU";
+//        user.location = location;
+//        user.name = "Bruce";
+
+
+
+//        webView.send("hello");
+
+	}
+
+	public void pickFile() {
+		Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
+		chooserIntent.setType("image/*");
+		startActivityForResult(chooserIntent, RESULT_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == RESULT_CODE) {
+			if (null == mUploadMessage){
+				return;
+			}
+			Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+			mUploadMessage.onReceiveValue(result);
+			mUploadMessage = null;
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (button.equals(v)) {
+            webView.callHandler("functionInJs", "data from Java", new CallBackFunction() {
+
+				@Override
+				public void onCallBack(String data) {
+					// TODO Auto-generated method stub
+					Log.i(TAG, "reponse data from js " + data);
+				}
+
+			});
+		}
+
+	}
+
+}
